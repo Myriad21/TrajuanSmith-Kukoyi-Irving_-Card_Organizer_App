41ad74f0fa3de785322024a25a1166354cf1a3e3
Trajuan <tr4juansmith@gmail.com>
2026-03-04 14:44:11 -0500

    Updated _loadingFOlders in folders screen to include card count instead of id'

--- lib/screens/folders_screen.dart
+++ lib/screens/folders_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/folder.dart';
 import '../repositories/folder_repository.dart';
+import '../repositories/card_repository.dart';
 
 class FoldersScreen extends StatefulWidget {
   const FoldersScreen({super.key});
@@ -11,8 +12,10 @@ class FoldersScreen extends StatefulWidget {
 
 class _FoldersScreenState extends State<FoldersScreen> {
   final FolderRepository _folderRepo = FolderRepository();
+  final CardRepository _cardRepo = CardRepository();
 
   List<Folder> _folders = [];
+  Map<int, int> _cardCounts = {};
   bool _loading = true;
 
   @override
@@ -24,8 +27,15 @@ class _FoldersScreenState extends State<FoldersScreen> {
   Future<void> _loadFolders() async {
     final folders = await _folderRepo.getAllFolders();
 
+    final Map<int, int> counts = {};
+    for (final folder in folders) {
+      final id = folder.id!;
+      counts[id] = await _cardRepo.getCardCountByFolder(id);
+    }
+
     setState(() {
       _folders = folders;
+      _cardCounts = counts;
       _loading = false;
     });
   }
@@ -42,7 +52,7 @@ class _FoldersScreenState extends State<FoldersScreen> {
               final folder = _folders[index];
               return ListTile(
                 title: Text(folder.folderName),
-                subtitle: Text('id: ${folder.id}'),
+                subtitle: Text('${_cardCounts[folder.id!] ?? 0} cards'),
               );
             },
           )
